ffb91e081579df45c0b6f7120784323552952cc3
Martin4TheWin <martin49.ek@gmail.com>
2022-03-25 10:57:53 +0200

    Homepage skeleton

---
+++ public/script.js
@@ -0,0 +1,9 @@
+
+  fetch('https://api.pandascore.co/leagues').then((res) => {
+    /*if (res.ok) {
+    console.log('SUCCESS');
+  } else {
+    console.log('Not Successful');
+  }*/
+  });
+  Brearer <0toZ1heZBQGb7vOjA_bF74zW7oOGr6puOyNvl_Scicu89FhM5-g0>
